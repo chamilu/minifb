920f04ddf249a235fde4ccd0a256455bc56108a3
Chamil Udayanga <chamiludayanga28@gmail.com>
2018-05-23 02:56:14 +0800

    modified webpack dev server configs - historyApiFallback

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,27 @@ const sourceDir = resolve(__dirname, './src');
 const publicDir = resolve(__dirname, './public');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: resolve(sourceDir, 'index.js'),
-  output: {
-    path: resolve(publicDir, './dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    mode: process.env.NODE_ENV || 'development',
+    entry: resolve(sourceDir, 'index.js'),
+    output: {
+        path: resolve(publicDir, './dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        port: 3000,
+        contentBase: './public',
+        historyApiFallback: true,
+    },
 };
