43bcf44258acc81382c527950932820273435f25
Chamil Udayanga <chamiludayanga28@gmail.com>
2018-05-25 14:19:56 +0530

    Added morgan package.

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Path = require('path');
-
+const morgan = require('morgan');
 const app = express();
+
 const publicDir = Path.resolve(__dirname, '../../public');
 const port = process.env.PORT || 4000;
 
+app.use(morgan('dev'));
 app.use(express.static(publicDir));
 
 app.get('*', (req, res) => {
