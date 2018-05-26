a276953cef2f75a6bed913804b447c0d7746b828
Chamil Udayanga <chamiludayanga28@gmail.com>
2018-05-26 07:23:53 +0530

    Refactor: refactor to merge to master

--- src/server/api/user.js
+++ src/server/api/user.js
@@ -4,18 +4,7 @@ const mongoose = require('mongoose');
 const User = require('../models/User');
 
 userRouter.route('/').get((req, res) => {
-    const newUser = new User({
-        _id: mongoose.Types.ObjectId(),
-        firstname: 'chamil',
-        lastname: 'udayanga',
-        email: 'chamilmcom@gmail.com',
-        passwotd: 'somegeneratedbspassword',
-        isActivated: false,
-    });
-
-    newUser.save((err, result) => {
-        res.send(JSON.stringify(result));
-    });
+    res.send('in user route');
 });
 
 module.exports = userRouter;
